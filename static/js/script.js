6f2b2a24aaad0af4610868f1c5fe35a57f87c9d5
BM2204 <mahajanbhushan2204@gmail.com>
2023-12-22 17:27:46 +0530

    Added Mobile

--- static/js/script.js
+++ static/js/script.js
@@ -2,33 +2,64 @@ let isDragging = false;
 let offsetX, offsetY;
 let draggedElement;
 
-document.querySelectorAll('.draggable').forEach((div) => {
-    div.addEventListener('mousedown', (e) => {
-        isDragging = true;
-        offsetX = e.clientX - div.getBoundingClientRect().left;
-        offsetY = e.clientY - div.getBoundingClientRect().top;
-        draggedElement = div;
-        draggedElement.classList.add('dragging');
-    });
-});
+function startDragging(e) {
+    isDragging = true;
 
-document.addEventListener('mousemove', (e) => {
+    if (e.type === 'mousedown') {
+        offsetX = e.clientX - draggedElement.getBoundingClientRect().left;
+        offsetY = e.clientY - draggedElement.getBoundingClientRect().top;
+    } else if (e.type === 'touchstart') {
+        offsetX = e.touches[0].clientX - draggedElement.getBoundingClientRect().left;
+        offsetY = e.touches[0].clientY - draggedElement.getBoundingClientRect().top;
+    }
+
+    draggedElement.classList.add('dragging');
+}
+
+function handleDragging(e) {
     if (!isDragging) return;
 
-    const x = e.clientX - offsetX;
-    const y = e.clientY - offsetY;
+    let x, y;
+
+    if (e.type === 'mousemove') {
+        x = e.clientX - offsetX;
+        y = e.clientY - offsetY;
+    } else if (e.type === 'touchmove') {
+        x = e.touches[0].clientX - offsetX;
+        y = e.touches[0].clientY - offsetY;
+    }
 
     draggedElement.style.left = `${x}px`;
     draggedElement.style.top = `${y}px`;
-});
+}
 
-document.addEventListener('mouseup', () => {
+function stopDragging() {
     if (isDragging) {
         isDragging = false;
         draggedElement.classList.remove('dragging');
     }
     draggedElement = null;
+}
+
+document.querySelectorAll('.draggable').forEach((div) => {
+    div.addEventListener('mousedown', (e) => {
+        draggedElement = div;
+        startDragging(e);
+    });
+
+    div.addEventListener('touchstart', (e) => {
+        e.preventDefault(); // Prevent default behavior to avoid issues with touch events
+        draggedElement = div;
+        startDragging(e);
+    });
 });
+
+document.addEventListener('mousemove', handleDragging);
+document.addEventListener('touchmove', handleDragging);
+
+document.addEventListener('mouseup', stopDragging);
+document.addEventListener('touchend', stopDragging);
+
 
 
 function showPopup() {
